9d7acc01555d3393dffd63298a53582c8a46238d
traizooo <michal.owsiejew23@gmail.com>
2024-07-19 21:39:56 +0200

    Commit

--- frontend/src/pages/Create.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import CreateEventForm from '../components/Event/CreateEvent'
-
-const Create = () => {
-  return <CreateEventForm/>
-}
-
-export default Create
--- frontend/src/pages/Event.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import EventDetails from '../components/Event/EventDetails'
-
-const Event = () => {
-  return <EventDetails/>
-}
-
-export default Event
