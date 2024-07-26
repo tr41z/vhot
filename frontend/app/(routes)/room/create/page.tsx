66af0e1aa9f836224de358b8ff250edb46facbac
traizooo <michal.owsiejew23@gmail.com>
2024-07-26 16:34:36 +0200

    Commit

--- frontend/app/(routes)/room/create/page.tsx
+++ frontend/app/(routes)/room/create/page.tsx
@@ -7,10 +7,10 @@ const CreateRoomPage = () => {
     <div className="flex flex-col justify-center items-center h-[70vh]">
       <div className="flex flex-col justify-center items-center mx-5">
         <div className="flex flex-col text-center">
-          <p className="uppercase text-base md:text-2xl mr-2 tracking-widest bg-gradient-to-b from-gray-800 via-gray-600 to-gray-500 bg-clip-text text-transparent">
+          <p className="uppercase text-base md:text-xl mr-2 tracking-widest bg-gradient-to-b from-gray-800 via-gray-600 to-gray-500 bg-clip-text text-transparent">
             Create a room by completing the form
           </p>
-          <p className="uppercase text-base md:text-2xl tracking-widest bg-gradient-to-b from-gray-800 via-gray-600 to-gray-500 bg-clip-text text-transparent">
+          <p className="uppercase text-base md:text-xl tracking-widest bg-gradient-to-b from-gray-800 via-gray-600 to-gray-500 bg-clip-text text-transparent">
             and share the event link or ID!
           </p>
         </div>
--- frontend/app/(routes)/room/link/page.tsx
+++ frontend/app/(routes)/room/link/page.tsx
@@ -6,10 +6,10 @@ const JoinFromLinkPage = () => {
     <div className="flex flex-col justify-center items-center h-[75vh]">
       <div className="flex flex-col justify-center items-center">
         <div className="flex flex-col text-center md:flex-row">
-          <p className="uppercase text-base md:text-lg mr-2 tracking-widest bg-gradient-to-b from-gray-800 via-gray-600 to-gray-500 bg-clip-text text-transparent">
+          <p className="uppercase text-base md:text-xl mr-2 tracking-widest bg-gradient-to-b from-gray-800 via-gray-600 to-gray-500 bg-clip-text text-transparent">
             Join any room by just providing ID
           </p>
-          <p className="uppercase text-base md:text-lg tracking-widest bg-gradient-to-b from-gray-800 via-gray-600 to-gray-500 bg-clip-text text-transparent">
+          <p className="uppercase text-base md:text-xl tracking-widest bg-gradient-to-b from-gray-800 via-gray-600 to-gray-500 bg-clip-text text-transparent">
             shared by the host
           </p>
         </div>
